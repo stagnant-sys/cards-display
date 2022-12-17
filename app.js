fc52b49120018eedf05a38acf93e95370844c09d
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2022-12-17 14:16:29 +0100

    Add header

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let backdrop = document.querySelector('.backdrop');
 let currentCardContainer = document.querySelector('.main-card__container');
 let currentCard = document.querySelector('.main-card__frame');
+let loggedInOptions = document.querySelector('.header-otions__loggedin');
+let loggedOffOptions = document.querySelector('.header-options__loggedoff');
+let loggedIn = false;
+
+if (!loggedIn) {
+  loggedInOptions.style.display = 'none';
+  loggedOffOptions.style.display = 'block';
+} else {
+  loggedInOptions.style.display = 'block';
+  loggedOffOptions.style.display = 'none';
+}
 
 currentCardContainer.addEventListener('click', function() {
   currentCard.classList.remove('animated-card');
